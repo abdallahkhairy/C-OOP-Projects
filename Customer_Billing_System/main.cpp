5f2a703406b6b0946bcdb8480eb318d53e67c849
abdallahkhairy <abdullahkhairy98@gmail.com>
2023-11-26 04:57:35 +0200

    Editing..

--- Customer_Billing_System/main.cpp
+++ Customer_Billing_System/main.cpp
@@ -8,23 +8,17 @@
 using namespace std;
 
 int main() {
-    // Sample items in the inventory
-    Item item1("Product A", 20.99, 4);
-    Item item2("Product B", 15.49, 2);
-    Item item3("Product C", 5.99, 10);
-
-    // Creating a shopping cart
     MyCart cart;
 
     int choice;
 
     do {
-        cout << endl <<"===== Customer Billing System ===== "<< endl;
+        cout << endl <<"===== Customer Billing System ===== "<< endl<<endl;
         cout << "1- Add Item to Your Cart "<< endl;
         cout << "2- View Cart items "<< endl;
         cout << "3- Display The Receipt "<< endl;
-        cout << "0- Quit "<< endl;
-        cout << "Enter your choice: ";
+        cout << endl << "To Quit Press 0 "<< endl;
+        cout << endl << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
@@ -38,7 +32,7 @@ int main() {
             cart.displayReceipt();
             break;
         case 0:
-            cout << "Exiting.." << endl;
+            cout << "Exiting..." << endl;
             break;
         default:
             cout << "Invalid choice" <<endl<< "Please try again" << endl;
